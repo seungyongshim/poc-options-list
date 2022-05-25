a35ed4e22cf91ba80505e8199ada8ac1804efc90
심승용 <seungyongshim@nexon.co.kr>
2022-05-25 13:28:45 +0900

    .

--- src/Sample.Tests/UnitTest1.cs
+++ src/Sample.Tests/UnitTest1.cs
@@ -1,14 +1,54 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.Toolkit.HighPerformance;
 using Xunit;
 using static Sample.Prelude;
 
 namespace Sample.Tests;
 
+public class Business
+{
+    public string S3Arn { get; set; }
+    public IList<string> SnsArn { get; set; }
+}
+
 public class PreludeSpec
 {
     [Fact]
-    public void AddSuccess()
+    public async Task OptionsSpec()
     {
-        Assert.Equal(3, add(1)(2));
+        using var appsettingsStream = Encoding.Default.GetBytes(@"
+        {
+            'Business' : {
+                'S3Arn' : 's3arn',
+                'SnsArn' : ['snsarn1', 'snsarn2'],
+                'Region' : 'ap-northeast-1'
+            }
+        }".Replace('\'', '\"')).AsMemory().AsStream();
+
+        var host = Host.CreateDefaultBuilder()
+                       .ConfigureAppConfiguration(config => config.AddJsonStream(appsettingsStream))
+                       .ConfigureServices(services =>
+                       {
+                           services.AddOptions<Business>()
+                                   .BindConfiguration("Business");
+                       })
+                       .Build();
+
+        await host.StartAsync();
+
+        var ret = host.Services.GetService<IOptions<Business>>();
+
+        Assert.Equal(2, ret.Value.SnsArn.Count());
+
+        await host.StopAsync();
+
     }
 }
